935d44e595f53cdde67c1f1f9658672ea14877a2
Samuel Anowai <109921144+Ikem98@users.noreply.github.com>
2024-04-22 21:20:23 -0600

    questions

---
+++ app/data.js
@@ -0,0 +1,10 @@
+export const quiz ={
+    totalQuestions: 5,
+    questions[
+
+            {id: 1,
+            question: 'What is the capital of France?',
+        answers: ['Madrid', 'Paris', 'Jupiter', 'Saturn'],
+    correctAnswer: 'Jupiter',},
+    ]
+}
