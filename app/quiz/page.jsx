2dd843fefdf4faf3a168b9757fb5a0734f191614
Samuel Anowai <109921144+Ikem98@users.noreply.github.com>
2024-04-22 21:43:41 -0600

    Update page.jsx

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -3,6 +3,11 @@ import React, {useState} from 'react'
 import { quiz } from '../data';
 
 const page = () => {
+    const [activeQuestion, setActiveQuestion] = useState(0);
+    const [selectedAnswer, setSelectedAnswer] = useState('');
+    const [ checked, setChecked] = useState(false);
+    const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+    const [showResult, setShowResult]
   return (
     <div className='container'>
         <h1>Quiz Page</h1>
