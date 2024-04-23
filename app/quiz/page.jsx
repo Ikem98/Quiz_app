9b5ec7b20448b0df03742bb23da596fdad1c9496
Samuel Anowai <109921144+Ikem98@users.noreply.github.com>
2024-04-22 21:47:43 -0600

    Update page.jsx

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -7,15 +7,24 @@ const page = () => {
     const [selectedAnswer, setSelectedAnswer] = useState('');
     const [ checked, setChecked] = useState(false);
     const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
-    const [showResult, setShowResult]
+    const [showResult, setShowResult] = useState(false);
+    const [result, setResult] = useState ({
+        score: 0,
+        correctAnswers: 0,
+        wrongAnswers: 0,
+    });
+
+    const {questions} = quiz;
+    const { question, answers, correctAnswer } = questions[activeQuestion];
+
   return (
     <div className='container'>
         <h1>Quiz Page</h1>
 
         <div>
             <h2>
-                Question: 1
-                <span> /5</span>
+                Question: {activeQuestion + 1}
+                <span> /{questions.length}</span>
             </h2>
         </div>
         
